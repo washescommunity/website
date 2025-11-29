8425f40029f97b6a3dbf015811f1e0516c741918
Estela Miranda Batista <estelambatista@gmail.com>
2025-11-29 14:41:03 -0300

    Atualizando Banner

--- src/components/BannerWASHES.jsx
+++ src/components/BannerWASHES.jsx
@@ -39,7 +39,7 @@ export function BannerWASHES({anoAtual, creditosDaImagem}){
         </div>
 
         <div className='w-full text-right pr-2 text-gray-500 text-sm md:block hidden'>
-          {"url(/assets/banners/banner-hero-WASHES2026.jpg)"}
+          {/* Adicione a referência da imagem aqui */"banner-hero-WASHES2026"}
           <span>{creditosDaImagem}</span>
         </div>
 
