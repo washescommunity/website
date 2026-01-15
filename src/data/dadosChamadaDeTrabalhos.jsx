8307e7cf93a4d9b8178b9feb2349b6a6cbe5cb99
Estela Miranda Batista <estelambatista@gmail.com>
2026-01-15 09:41:39 -0300

    Adicionando instrução de submissão

--- src/data/dadosChamadaDeTrabalhos.jsx
+++ src/data/dadosChamadaDeTrabalhos.jsx
@@ -293,7 +293,15 @@ export const dadosChamadaDeTrabalho = {
 2026: [
   {
     titulo: "Instrução para Submissão",
-    conteudo: `Mais informações em breve.`,
+    conteudo: `
+    Todas as submissões devem seguir o estilo de formatação dos eventos da Sociedade Brasileira de Computação (SBC) e serem enviadas por meio da plataforma JEMS3: <a href="https://jems3.sbc.org.br/washes2026" target="_blank"> <u> https://jems3.sbc.org.br/washes2026 </u> </a>.
+    As submissões devem ser realizadas de acordo com o sistema duplo-anônimo (double-anonymous), omitindo-se qualquer identificação de seus autores, bem como de seus revisores. Artigos que não atenderem ao sistema de duplo-anônimo serão rejeitados.
+    Artigos aprovados pelo comitê de programa serão publicados nos anais do evento.
+
+    <b>Categoria de Artigos</b>
+    <li>Artigos completos (até 10 páginas, com limite de duas páginas adicionais para referências), apresentando resultados de interesse para o avanço da pesquisa na área. Artigos que descrevem modelos, métodos, estudos experimentais e relatos de experiência industrial são bem-vindos.</li>
+    <li>Artigos Curtos (até 5 páginas, com limite de uma página adicional para referências), descrevendo trabalhos em andamento, ou uma visão de um projeto de pesquisa, ferramenta ou técnica, ou trabalhos explorando perspectivas de pesquisa nos temas do workshop. Este tipo de artigo será avaliado seguindo os mesmos critérios da categoria “artigos completos”, tendo como peso o potencial de discussão que o artigo pode gerar no workshop.</li>
+    `,
   },
   {
     titulo: "Datas Importantes",
