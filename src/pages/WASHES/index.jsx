28a0f8881b06f9d3b40cfa025e014bd3e6bd7713
Estela Miranda Batista <estelambatista@gmail.com>
2026-07-29 13:39:39 -0300

    resolvendo bug no deploy 3.0

--- src/pages/WASHES/index.jsx
+++ src/pages/WASHES/index.jsx
@@ -1,4 +1,4 @@
-export { default as Washes2026 } from "./Washes2027";
+export { default as Washes2027 } from "./Washes2027";
 export { default as Washes2026 } from "./Washes2026";
 export { default as Washes2025 } from "./Washes2025";
 export { default as Washes2024 } from "./Washes2024";
